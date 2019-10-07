38167e58492c36c006f6dd105f1c2dd0ba8bebcb
Bohdan Lysov <BogUkraine@gmail.com>
2019-10-07 22:50:06 +0300

    Update assignment02a.py

--- assignment02a.py
+++ assignment02a.py
@@ -10,7 +10,29 @@ True
 """
 
 def poem():
-    return ''
+    array = [['the house that Jack built.', 'lay in the house that Jack built.'],
+             ['the malt', 'ate the malt'],
+             ['the rat,', 'killed the rat,'],
+             ['the cat,', 'worried the cat,'],
+             ['the dog,', 'tossed the dog,'],
+             ['the cow with the crumpled horn,', 'milked the cow with the crumpled horn,'],
+             ['the maiden all forlorn,', 'kissed the maiden all forlorn,'],
+             ['the man all tattered and torn,','married the man all tattered and torn,'],
+             ['the priest all shaven and shorn,','waked the priest all shaven and shorn,'],
+             ['the cock that crowed in the morn,','kept the cock that crowed in the morn,'],
+             ['the farmer sowing his corn,','']]
+
+    res = ''
+
+    for i in range (len(array)):
+        for j in range(i+1):
+            if j == 0:
+                res += 'This is ' + array[i-j][0]
+            else:
+                res += 'That ' + array[i-j][1]
+            res += '\n'
+        res += '\n'
+    return res[:-1]
 
 
 if __name__ == '__main__':
